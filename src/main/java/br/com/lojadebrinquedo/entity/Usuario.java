578d11565178e552a1f01b321bbe18792d636c3d
Nathan Nunes Calsonari <130010914+NathanNunesCalsonari@users.noreply.github.com>
2024-10-11 15:39:36 -0300

    Add files via upload

---
+++ src/main/java/br/com/lojadebrinquedo/entity/Usuario.java
@@ -0,0 +1,25 @@
+package br.com.lojadebrinquedo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "T_LDB_USUARIO")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_USUARIO")
+    private Long id;
+
+    @Column(name = "USERNAME", unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "PASSWORD", nullable = false)
+    private String password;
+}
